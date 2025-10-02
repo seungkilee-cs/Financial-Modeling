c15b23372bb8eb7c00456fa20f529b0d076c48a3
Seung Ki Lee <seungkilee.cs@gmail.com>
2025-10-02 21:16:11 +0900

    changed the vite config

--- ts-visualizer/vite.config.ts
+++ ts-visualizer/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/financial-modeling/",
+  base: "/Financial-Modeling/",
   build: {
     outDir: "../dist",
   },
